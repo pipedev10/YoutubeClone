bcfafbc0ab938b6b592f5f4cbe5f4782f55359f5
Felipe Carrasco <pipecarrasco@MacBook-Pro-de-Felipe.local>
2022-11-29 20:50:54 -0300

    clase nro 2

---
+++ YoutubeClone/YoutubeClone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  YoutubeClone
+//
+//  Created by Felipe Carrasco on 29-11-22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
