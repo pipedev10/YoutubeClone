6e29acd9800d1730c992111ff120d7b4994b8f58
Felipe Carrasco <pipecarrasco@MacBook-Pro-de-Felipe.local>
2022-12-05 08:18:39 -0300

    <clase-7-8-9> service layer and model

---
+++ YoutubeClone/YoutubeClone/Utils/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  YoutubeClone
+//
+//  Created by Felipe Carrasco on 05-12-22.
+//
+
+import Foundation
+
+struct Constants {
+    static let apiKey = "AIzaSyDanj-NW6AOO5MYYegkMBernD5tO_-Rk40"
+}
