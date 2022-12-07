364c18c87acb79be22610e7489661fbec4e18ea0
Felipe Carrasco <pipecarrasco@MacBook-Pro-de-Felipe.local>
2022-12-07 00:03:23 -0300

    <clase 15> crear mocks

---
+++ YoutubeClone/YoutubeClone/Home/Model/Provider/HomeProviderMock.swift
@@ -0,0 +1,41 @@
+//
+//  HomeProviderMock.swift
+//  YoutubeClone
+//
+//  Created by Felipe Carrasco on 06-12-22.
+//
+
+import Foundation
+
+class HomeProviderMock: HomeProviderProtocol {
+    func getVideos(searchString: String, channelId: String) async throws -> VideoModel {
+        guard let model = Utils.parseJson(jsonName: "SearchVideos", model: VideoModel.self) else {
+            throw NetworkError.jsonDecoder
+        }
+        return model
+    }
+    
+    func getChannel(channelId: String) async throws -> ChannelModel {
+        guard let model = Utils.parseJson(jsonName: "Channel", model: ChannelModel.self) else {
+            throw NetworkError.jsonDecoder
+        }
+        return model
+    }
+    
+    func getPlaylists(channelId: String) async throws -> PlaylistModel {
+        guard let model = Utils.parseJson(jsonName: "Playlists", model: PlaylistModel.self) else {
+            throw NetworkError.jsonDecoder
+        }
+        return model
+    }
+    
+    func getPlaylistItems(playlistId: String) async throws -> PlaylistItemsModel {
+        guard let model = Utils.parseJson(jsonName: "PlaylistItems", model: PlaylistItemsModel.self) else {
+            throw NetworkError.jsonDecoder
+        }
+        return model
+    }
+    
+
+    
+}
