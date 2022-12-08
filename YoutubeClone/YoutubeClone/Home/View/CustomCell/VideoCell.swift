0a68732024a4fbb1999b152deaeb473e3c617fda
Felipe Carrasco <pipecarrasco@MacBook-Pro-de-Felipe.local>
2022-12-08 14:25:23 -0300

    <clase-16> feature: add data table

---
+++ YoutubeClone/YoutubeClone/Home/View/CustomCell/VideoCell.swift
@@ -0,0 +1,23 @@
+//
+//  VideoCell.swift
+//  YoutubeClone
+//
+//  Created by Felipe Carrasco on 07-12-22.
+//
+
+import UIKit
+
+class VideoCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
--- YoutubeClone/YoutubeClone/Home/View/HomeViewController.swift
+++ YoutubeClone/YoutubeClone/Home/View/HomeViewController.swift
@@ -9,21 +9,81 @@ import UIKit
 
 class HomeViewController: UIViewController {
 
+    @IBOutlet weak var tableViewHome: UITableView!
     lazy var presenter = HomePresenter(delegate: self)
+    private var objectList: [[Any]] = []
+    private var sectionTitleList: [String] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        configTableView()
         Task {
             await presenter.getHomeObjects()
         }
     }
+    
+    func configTableView() {
+        let nibChannel = UINib(nibName: "\(ChannelCell.self)", bundle: nil)
+        tableViewHome.register(nibChannel, forCellReuseIdentifier: "\(ChannelCell.self)")
+        
+        let nibVideo = UINib(nibName: "\(VideoCell.self)", bundle: nil)
+        tableViewHome.register(nibVideo, forCellReuseIdentifier: "\(VideoCell.self)")
+        
+        let nibPlaylist = UINib(nibName: "\(PlaylistCell.self)", bundle: nil)
+        tableViewHome.register(nibPlaylist, forCellReuseIdentifier: "\(PlaylistCell.self)")
+        
+        tableViewHome.delegate = self
+        tableViewHome.dataSource = self
+    }
 
+}
+
+extension HomeViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return objectList[section].count
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return objectList.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let item = objectList[indexPath.section]
+        if let channel = item as? [ChannelModel.Items]{
+            guard let channelCell = tableView.dequeueReusableCell(withIdentifier: "\(ChannelCell.self)", for: indexPath) as? ChannelCell else {
+                return UITableViewCell()
+            }
+            return channelCell
+        } else if let playlistItems = item as? [PlaylistItemsModel.Item]{
+            guard let playlistItemCell = tableView.dequeueReusableCell(withIdentifier: "\(VideoCell.self)", for: indexPath) as? VideoCell else {
+                return UITableViewCell()
+            }
+            return playlistItemCell
+        } else if let videos = item as? [VideoModel.Item]{
+            guard let videoCell = tableView.dequeueReusableCell(withIdentifier: "\(VideoCell.self)", for: indexPath) as? VideoCell else {
+                return UITableViewCell()
+            }
+            return videoCell
+        } else if let playlist = item as? [PlaylistModel.Item]{
+            guard let playlistCell = tableView.dequeueReusableCell(withIdentifier: "\(PlaylistCell.self)", for: indexPath) as? PlaylistCell else {
+                return UITableViewCell()
+            }
+            return playlistCell
+        }
+        
+        return UITableViewCell()
+    }
+    
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return sectionTitleList[section]
+    }
 
 }
 
 extension HomeViewController: HomeViewProtocol {
-    func getData(list: [[Any]]) {
-        print("list: ", list)
+    func getData(list: [[Any]], sectionTitleList: [String]) {
+        objectList = list
+        self.sectionTitleList = sectionTitleList
+        tableViewHome.reloadData()
     }
 }
